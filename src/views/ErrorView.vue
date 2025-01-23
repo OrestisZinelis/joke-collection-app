bb1c246fe6dca507881a7ee780055fb502bee2b4
Orestis Zinelis <zinelisorestis@gmail.com>
2025-01-23 18:23:55 +0200

    Config Router, Add ErrorView & empty Views

---
+++ src/views/ErrorView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex flex-col items-center justify-center h-screen text-center">
+    <h1 class="text-4xl font-bold text-red-500 mb-4">404</h1>
+    <p class="text-xl">Page Not Found</p>
+    <router-link to="/" class="btn-primary mt-4">Add a joke</router-link>
+    <router-link to="/collection" class="btn-primary mt-4">Check collection</router-link>
+  </div>
+</template>
